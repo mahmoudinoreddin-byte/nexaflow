674c4aca730c024d0bb61d3a8f92176f542f5be6
mahmoudinoreddin-byte <mahmoudinoreddin@gmail.com>
2026-04-03 08:35:59 +0100

    Delete apps directory

--- apps/services/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from .models import Service
-
-
-@login_required
-def service_list(request):
-    services = Service.objects.filter(status=Service.STATUS_ACTIVE)
-    return render(request, 'dashboard/services.html', {'services': services})
