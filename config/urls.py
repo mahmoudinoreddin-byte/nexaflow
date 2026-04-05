78b74bae2d93ddbc70b9c0c8adaf4dab2d0b6d50
mahmoudinoreddin-byte <mahmoudinoreddin@gmail.com>
2026-04-05 19:10:56 +0100

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from apps.dashboard.views import landing_page
-from tool_app import sketch_fabric
+from tool_app import sketch-fabric
 
 
 urlpatterns = [
